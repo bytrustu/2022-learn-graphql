68be4f66f1f6d24471cc1bfc5c370166c1f52107
bytrustu <bytrustu@gmail.com>
2022-01-19 17:11:46 +0900

    [ADD] Client 메세지 CRUD 구현

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,11 @@
+import MsgList from "../components/MsgList";
+
 const Home = () => {
     return (
         <>
             <h1>SIMPLE SNS</h1>
+            <br />
+            <MsgList />
         </>
     )
 }
